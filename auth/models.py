411642d912858488c7fec5b672e769a57890cd54
Reni Stephen <renistephen2@gmail.com>
2025-04-20 14:57:39 -0400

    changed mysql connection using `mysql-connector-python`

--- auth/models.py
+++ auth/models.py
@@ -1,14 +1,36 @@
-from auth import mysql
+#from auth import mysql
+from auth.db import get_connection
+
+# def find_user_by_email(email):
+#     cursor = mysql.connection.cursor()
+#     cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
+#     user = cursor.fetchone()
+#     cursor.close()
+#     return user
+
 
 def find_user_by_email(email):
-    cursor = mysql.connection.cursor()
+    conn = get_connection()
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM users WHERE email = %s", (email,))
     user = cursor.fetchone()
     cursor.close()
+    conn.close()
     return user
 
+# def create_user(name, email, hashed_pw):
+#     cursor = mysql.connection.cursor()
+#     cursor.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, hashed_pw))
+#     mysql.connection.commit()
+#     cursor.close()
+
 def create_user(name, email, hashed_pw):
-    cursor = mysql.connection.cursor()
-    cursor.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (name, email, hashed_pw))
-    mysql.connection.commit()
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(
+        "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)",
+        (name, email, hashed_pw)
+    )
+    conn.commit()
     cursor.close()
+    conn.close()
--- config.py
+++ config.py
@@ -89,10 +89,14 @@ app.config["SESSION_KEY_PREFIX"] = "session:"
 
 #Database config
 #SECRET_KEY = os.getenv('SECRET_KEY')
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+# app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+# app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+# app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+# app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
+MYSQL_HOST = os.getenv('MYSQL_HOST')
+MYSQL_USER = os.getenv('MYSQL_USER')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+MYSQL_DB = os.getenv('MYSQL_DB')
 
 Session(app)  # Initialize session extension
